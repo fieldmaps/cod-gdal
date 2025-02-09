5223431c60bb7664b8b03188e05a4d49888ccdb7
Max Malynowsky <github@maxmalynowsky.com>
2025-02-09 21:44:36 +0100

    use slim docker

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 from .routers import health, ogr2ogr
 
 app = FastAPI()
-app.include_router(health.router)
 app.include_router(ogr2ogr.router)
+app.include_router(health.router)
